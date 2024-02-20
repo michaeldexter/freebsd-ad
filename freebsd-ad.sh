7f49c1e1563b2cfcf83727c50fed67ae00036e09
michaeldexter <editor@callfortesting.org>
2024-02-19 22:38:55 -0800

    Finally some love

--- freebsd-ad.sh
+++ freebsd-ad.sh
@@ -1,33 +1,46 @@
 #!/bin/sh
 
-# Copyright 2021 Michael Dexter. All rights reserved
+# Copyright 2021, 2024 Michael Dexter. All rights reserved
  
-which samba || \
-{ echo Samba missing - pkg install samba413 ldb22 bind-tools ; exit 1 ; }
+# v.1
 
-echo Killing samba smbd nmbd winbindd
+pkg search samba
+
+echo ; echo Enter a Samba version to install i.e. samba419
+read samba_version
+
+pkg install $samba_version
+#samba419 and ldb22 are incompatible, but, supplanted by samba-ldbedit?
+#which ldbedit || pkg install ldb22
+#which nslookup || pkg install bind-tools
+
+echo ; echo Killing samba smbd nmbd winbindd
 #pkill samba smbd nmbd winbindd
 service samba_server onestop
 
-echo Removing previous configuration
-rm -rf /usr/local/etc/smb4.conf /var/db/samba4/*
+echo ; echo Renaming previous smb4.conf and krb5.conf if present
+[ -f /usr/local/etc/smb4.conf ] && mv /usr/local/etc/smb4.conf /usr/local/etc/smb4.conf.previous
+[ -f /usr/local/etc/krb5.conf ] && mv /usr/local/etc/krb5.conf /usr/local/etc/krb5.conf.previous
 
-echo Removing previous certificates
+echo ; echo Deleting /var/db/samba4/*
+rm -rf /var/db/samba4/*
+
+echo ; echo Removing previous certificates
 [ -f /var/db/samba4/private/tls/key.pem ] && \
 	rm /var/db/samba4/private/tls/key.pem
 [ -f /var/db/samba4/private/tls/cert.pem ] && \
 	rm /var/db/samba4/private/tls/cert.pem
 
-echo Enter a domain such as AD
+echo ; echo Enter a domain such as DC:
 read domain
 
-echo Enter a realm such as TESTDOMAIN.COM
+echo ; echo Enter a realm such as MYDOMAIN.LOCAL:
 read realm
 
-echo Enter an administraor password \(will echo\)
+echo ; echo Enter a strong administraor password \(will echo and be tested\)
 read adminpass
 
-echo Provisioning with samba-tool domain provision --use-rfc2307 \
+echo ; echo Provisioning with samba-tool domain provision --use-rfc2307 \
 	 --realm=${domain}.$realm --domain=$domain \
 	 --server-role=dc --adminpass $adminpass
 
@@ -50,20 +63,20 @@ samba-tool domain provision --use-rfc2307 \
 [ -f /usr/local/etc/smb4.conf ] || \
 	{ echo /usr/local/etc/smb4.conf failed to generate ; exit 1 ; }
 
-echo The resulting /usr/local/etc/smb4.conf reads
+echo ; echo The resulting /usr/local/etc/smb4.conf reads
 cat /usr/local/etc/smb4.conf
 
-echo Copying generated krb5.conf to /usr/local/etc/krb5.conf
+echo ; echo Copying generated krb5.conf to /usr/local/etc/krb5.conf
 
 cp /usr/local/share/samba*/setup/krb5.conf /usr/local/etc/krb5.conf
 
-echo The resulting /usr/local/etc/krb5.conf reads
+echo ; echo The resulting /usr/local/etc/krb5.conf reads
 cat /usr/local/etc/krb5.conf
 
-echo Creating /var/db/samba4/private/tls if missing
+echo ; echo Creating /var/db/samba4/private/tls if missing
 [ -d /var/db/samba4/private/tls ] || mkdir -p /var/db/samba4/private/tls
 
-echo Generating TLS keys and certificates
+echo ; echo Generating TLS keys and certificates
 
 # INTERACTIVE without -subj
 openssl req -newkey rsa:2048 -keyout \
@@ -74,19 +87,19 @@ openssl req -newkey rsa:2048 -keyout \
 [ $? = 0 ] || \
 	{ echo Key generation failed according to the return value ; exit 1 ; }
 
-echo Running chmod 600 /var/db/samba4/private/tls/key.pem
+echo ; echo Running chmod 600 /var/db/samba4/private/tls/key.pem
 chmod 600 /var/db/samba4/private/tls/key.pem
 
 # Validation step... sample syntax
 # https://support.acquia.com/hc/en-us/articles/360004119234-Verifying-the-validity-of-an-SSL-certificate
 
-echo /var/db/samba4/private/tls/cert.pem signature is
+echo ; echo /var/db/samba4/private/tls/cert.pem signature is
 openssl x509 -in cert.pem -noout -pubkey
 
-echo /var/db/samba4/private/tls/key.pem signature is
+echo ; echo /var/db/samba4/private/tls/key.pem signature is
 openssl rsa -in key.pem -pubout
 
-echo Adding keys to /usr/local/etc/smb4.conf
+echo ; echo Adding keys to /usr/local/etc/smb4.conf
 
 sed -i'' -e '/workgroup/a\
         tls enabled  = yes\
@@ -98,14 +111,14 @@ sed -i'' -e '/workgroup/a\
 [ $? = 0 ] || \
 	{ echo Key addition failed according to the return value ; exit 1 ; }
 
-echo The resulting /usr/local/etc/smb4.conf reads
+echo ; echo The resulting /usr/local/etc/smb4.conf reads
 cat /usr/local/etc/smb4.conf
 
 #echo Verifying that /var/db/samba4/bind-dns/named.conf was generated
 #[ -f /var/db/samba4/bind-dns/named.conf ] || \
 #	{ echo named.conf failed to generate ; exit 1 ; }
 
-echo Generating a resolv.conf that points at 127.0.0.1 and 8.8.8.8
+echo ; echo Generating a resolv.conf that points at 127.0.0.1 and 8.8.8.8
 
 #echo search localdomain > /etc/resolv.conf
 echo nameserver 127.0.0.1 > /etc/resolv.conf
@@ -115,26 +128,28 @@ echo /etc/resolv.conf reads
 cat /etc/resolv.conf
 
 echo
-echo From here enable samba_server with:
+echo From here temporarily enable samba_server with:
 echo
-echo service onestart samba_server
+echo service samba_server onestart
 echo
 echo Various test and configuration tools:
 echo samba-tool dns query localhost ${domain}.${realm} @ ALL -U administrator
 echo samba-tool fsmo show
 echo samba_dnsupdate --verbose --all-names
-echo nslookup ${domain}.${realm}
+#echo nslookup ${domain}.${realm}
 echo kinit administrator@${domain}.${realm}
-echo klist -v
+#echo klist -v
 echo smbclient -L //localhost -U administrator
 echo samba-tool user create testuser
 echo wbinfo -i testuser
 echo wbinfo --name-to-sid testuser
-echo ldbedit...
+echo smbclient -L //localhost -U testuser
+echo wbinfo -t
+echo samba-ldbedit...
 echo samba-tool user list
 echo samba-tool dbcheck
 echo testparm -s
+echo smbd -b
 echo
 
 exit 0
-
